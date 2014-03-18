f1d22f1be9e921ed0de040fd41489eceaf87134f
David Oram <david.oram99@gmail.com>
2014-03-18 17:14:34 +1300

    Added API call to get a document

--- api/lib/api.rb
+++ api/lib/api.rb
@@ -1,5 +1,9 @@
 class API < Grape::API
   
+  # Specify verion number in http header eg:
+  #
+  #   curl -H Accept:application/vnd.phoenix-v1+json http://localhost/api/ping
+  #
   version 'v1', using: :header, vendor: 'phoenix'
   
   desc "Ping the server to check it is online, no authentication required"
@@ -7,4 +11,31 @@ class API < Grape::API
     { message: "pong" }
   end
   
+  desc "Return the data need to render a document"
+  get '/api/document/:id' do 
+		{ 
+      [
+        {
+          value: : {
+      			key:      20010,
+      			type:     "Project",
+      			column:   "projectKey",
+      			value:    "CONT-123-ABC"
+      			dataType: "string",
+      			label:    "Project key"
+      		},
+      		layout: {
+      			component: "read-only-string",
+      			bootstrap:  {
+      				row:              1,
+      				"column-start" :  4,
+      				"column-span" :   3,
+      				type:             "text", # HTML type 
+      				classes:          []      # Extra bootstrap classes to apply 
+      			}
+          }
+        }
+      ]
+		}
+  end
 end
